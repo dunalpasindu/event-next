bdf022bebe71f818097e3f928a1f152a4c34b329
dunalpasindu <inbox.dunal@gmail.com>
2025-05-01 11:02:05 +0530

    feat: finalize Event Accommodation Booking functionality

--- event-next/src/data/events.js
+++ event-next/src/data/events.js
@@ -27,7 +27,7 @@ export const events = [
         id: "acc1",
         name: "TechConf Hotel Room",
         price: 100,
-        image: "https://media.cnn.com/api/v1/images/stellar/prod/140127103345-peninsula-shanghai-deluxe-mock-up.jpg?q=w_2226,h_1449,x_0,y_0,c_fill"
+        image: "https://www.crownsydney.com.au/getmedia/4e33e654-2d0c-47d9-a76f-676dbea39659/220403-Crown-Sydney-Hotels-Executive-Suite-Living-1800x1200.jpg?height=1199"
       },
       {
         id: "acc2",
@@ -39,49 +39,43 @@ export const events = [
         id: "acc5",
         name: "TechConf Executive Suite",
         price: 200,
-        image: "https://www.hoteliermiddleeast.com/cloud/2022/09/Executive-Suite.jpg"
-      },
-      {
-        id: "acc6",
-        name: "TechConf Budget Room",
-        price: 30,
-        image: "https://www.budgetinn.com/images/rooms/standard-room.jpg"
+        image: "https://media.cnn.com/api/v1/images/stellar/prod/140127103345-peninsula-shanghai-deluxe-mock-up.jpg?q=w_2226,h_1449,x_0,y_0,c_fill"
       },
       {
         id: "acc13",
         name: "TechConf Deluxe Room",
         price: 150,
-        image: "https://www.deluxerooms.com/images/deluxe-room.jpg"
+        image: "https://sec.solaireresort.com/sites/default/files/2024-07/deluxe_5.webp"
       },
       {
         id: "acc14",
         name: "TechConf Studio Apartment",
         price: 120,
-        image: "https://www.studioapartments.com/images/studio-apartment.jpg"
+        image: "https://d28pk2nlhhgcne.cloudfront.net/assets/app/uploads/sites/3/2022/11/modern-studio-apartment-1220x671.jpg"
       },
       {
         id: "acc15",
         name: "TechConf Penthouse",
         price: 300,
-        image: "https://www.penthouses.com/images/penthouse.jpg"
+        image: "https://www.paranych.com/uploads/benefits-penthouse-living-main-image.png"
       },
       {
         id: "acc16",
         name: "TechConf Family Suite",
         price: 180,
-        image: "https://www.familysuites.com/images/family-suite.jpg"
+        image: "https://twoseasonsresorts.com/boracay/wp-content/uploads/2017/02/Family-Grand-Suite-1.jpg"
       },
       {
         id: "acc17",
         name: "TechConf Single Room",
         price: 80,
-        image: "https://www.singlerooms.com/images/single-room.jpg"
+        image: "https://i0.wp.com/vuniversity.in/wp-content/uploads/2023/10/Types-of-room-single.png?fit=600%2C375&ssl=1"
       },
       {
         id: "acc18",
         name: "TechConf Double Room",
         price: 110,
-        image: "https://www.doublerooms.com/images/double-room.jpg"
+        image: "https://www.pearlhotelnyc.com/hs-fs/hubfs/2.jpg?width=992&height=661&name=2.jpg"
       }
     ]
   },
@@ -125,49 +119,43 @@ export const events = [
         id: "acc7",
         name: "Festival Deluxe Room",
         price: 120,
-        image: "https://www.deluxerooms.com/images/deluxe-room.jpg"
+        image: "https://arenatours.com/wp-content/uploads/united-arab-emirates/inter-continental-dubai-festival-city/superior-room/superior-room-0.jpeg"
       },
       {
         id: "acc8",
         name: "Festival Shared Cabin",
         price: 60,
-        image: "https://www.sharedcabins.com/images/shared-cabin.jpg"
+        image: "https://powertraveller.com/wp-content/uploads/2024/12/1_2d-1n-isla-diablo-san-blas-in-shared-cabin-tour-meals-included.jpg"
       },
       {
         id: "acc19",
         name: "Festival Economy Room",
         price: 90,
-        image: "https://www.economyrooms.com/images/economy-room.jpg"
+        image: "https://www.queensangeles.com/media/k2/galleries/219/eco_01.jpg"
       },
       {
         id: "acc20",
         name: "Festival Artist Suite",
         price: 250,
-        image: "https://www.artistsuites.com/images/artist-suite.jpg"
+        image: "https://d3n14jmbdg5y6n.cloudfront.net/wp-content/uploads/2018/10/1504_1-1024x683.png"
       },
       {
         id: "acc21",
         name: "Festival Family Tent",
         price: 70,
-        image: "https://www.familytents.com/images/family-tent.jpg"
+        image: "https://outdoorroadie.co.uk/cdn/shop/files/olpro_home_5_man_inflatable_family_tent-13_800x800.jpg?v=1707141425"
       },
       {
         id: "acc22",
         name: "Festival Glamping Pod",
         price: 140,
-        image: "https://www.glampingpods.com/images/glamping-pod.jpg"
+        image: "https://www.pitchup.com/images/2/image/private/s--WhelKJz0--/c_limit,h_2400,w_3200/e_improve,fl_progressive/q_auto/b_rgb:000,g_south_west,l_pitchup.com_wordmark_white_watermark,o_15/v1629807986/rid3bzwnlqgjd7bgobhf.jpg"
       },
       {
         id: "acc23",
         name: "Festival Caravan",
         price: 160,
-        image: "https://www.caravans.com/images/caravan.jpg"
-      },
-      {
-        id: "acc24",
-        name: "Festival Bungalow",
-        price: 180,
-        image: "https://www.bungalows.com/images/bungalow.jpg"
+        image: "https://cdn.eventplanner.net/imgs/adv-6615/11816-img-desktop-festivalcaravans@2x.jpg"
       }
     ]
   },
@@ -179,61 +167,55 @@ export const events = [
         id: "acc9",
         name: "Summit Business Room",
         price: 150,
-        image: "https://www.businessrooms.com/images/business-room.jpg"
+        image: "https://smartway2.com/wp-content/uploads/2022/02/Smartway2-Conference-room.jpeg"
       },
       {
         id: "acc10",
         name: "Summit Economy Room",
         price: 80,
-        image: "https://www.economyrooms.com/images/economy-room.jpg"
+        image: "https://www.thebusinesscentreonline.co.uk/wp-content/uploads/2024/11/DSC09858-1-scaled.jpg"
       },
       {
         id: "acc25",
         name: "Summit Executive Suite",
         price: 220,
-        image: "https://www.executivesuites.com/images/executive-suite.jpg"
+        image: "https://assets.executivecentre.com/assets/banner-cityproduct-meetingroom.jpg"
       },
       {
         id: "acc26",
         name: "Summit Shared Workspace",
         price: 60,
-        image: "https://www.sharedworkspace.com/images/shared-workspace.jpg"
+        image: "https://www.harbortouchpossoftware.com/wp-content/uploads/2022/12/shared-workspace-example.jpg"
       },
       {
         id: "acc27",
         name: "Summit Penthouse",
         price: 300,
-        image: "https://www.penthouses.com/images/penthouse.jpg"
-      },
-      {
-        id: "acc28",
-        name: "Summit Studio Apartment",
-        price: 120,
-        image: "https://www.studioapartments.com/images/studio-apartment.jpg"
+        image: "https://www.arabianbusiness.com/wp-content/uploads/sites/3/cloud/2023/12/07/Como-Residences-Penthouse-Nakheel-3.jpg"
       },
       {
         id: "acc29",
         name: "Summit Family Suite",
         price: 180,
-        image: "https://www.familysuites.com/images/family-suite.jpg"
+        image: "https://c1.wallpaperflare.com/preview/1000/267/821/architecture-business-chairs-conference-room.jpg"
       },
       {
         id: "acc30",
         name: "Summit Single Room",
         price: 90,
-        image: "https://www.singlerooms.com/images/single-room.jpg"
+        image: "https://tenav.co.uk/wp-content/uploads/2023/10/banner-hero-01-1024x512.jpg"
       },
       {
         id: "acc31",
         name: "Summit Double Room",
         price: 130,
-        image: "https://www.doublerooms.com/images/double-room.jpg"
+        image: "https://smartsystems.imgix.net/Dual-Monitor-Conference-Room/Dual-Monitor-Conference-Room%20(1).jpg?w=1200&fit=crop"
       },
       {
         id: "acc32",
         name: "Summit Deluxe Room",
         price: 170,
-        image: "https://www.deluxerooms.com/images/deluxe-room.jpg"
+        image: "https://www.executivecentre.com/_next/image/?url=https%3A%2F%2Fassets.executivecentre.com%2Fassets%2FMeetingRoom-Rental_Hire.webp&w=3840&q=75"
       }
     ]
   },
@@ -245,61 +227,55 @@ export const events = [
         id: "acc11",
         name: "Expo Artist Suite",
         price: 250,
-        image: "https://www.artistsuites.com/images/artist-suite.jpg"
+        image: "https://www.outdoorpainter.com/wp-content/uploads/2024/07/11-Hughes-Easel-and-rolling-toolbox-from-Harbour-Freight-tools-scaled.jpg"
       },
       {
         id: "acc12",
         name: "Expo Standard Room",
         price: 100,
-        image: "https://www.standardrooms.com/images/standard-room.jpg"
+        image: "https://image-tc.galaxy.tf/wijpeg-9pswm2wt1iw7a8h35wghzbf0z/unahotels-expo-fiera-milano-monolocale-salotto-e-camera_standard.jpg?crop=56%2C0%2C889%2C667"
       },
       {
         id: "acc33",
         name: "Expo Deluxe Room",
         price: 150,
-        image: "https://www.deluxerooms.com/images/deluxe-room.jpg"
+        image: "https://m.ahstatic.com/is/image/accorhotels/HCM_P_3629866:4by3?fmt=jpg&op_usm=1.75,0.3,2,0&resMode=sharp2&iccEmbed=true&icc=sRGB&dpr=on,1.5&wid=335&hei=251&qlt=80"
       },
       {
         id: "acc34",
         name: "Expo Economy Room",
         price: 80,
-        image: "https://www.economyrooms.com/images/economy-room.jpg"
+        image: "https://q-xx.bstatic.com/xdata/images/hotel/max500/650103476.jpg?k=40a95e2c8a6df38a8e83bda22f519fe176d560d7cdb556ae25817744411e2d60&o="
       },
       {
         id: "acc35",
         name: "Expo Penthouse",
         price: 300,
-        image: "https://www.penthouses.com/images/penthouse.jpg"
+        image: "https://resource.rentcafe.com/image/upload/x_0,y_0,w_2400,h_1600,c_crop/q_auto,f_auto,c_fill,w_576,ar_1.5,g_auto/s3/2/89791/expo%20minneapolis%20apartment%20interiors-06(1).jpg"
       },
       {
         id: "acc36",
         name: "Expo Studio Apartment",
         price: 120,
-        image: "https://www.studioapartments.com/images/studio-apartment.jpg"
+        image: "https://www.idesignarch.com/wp-content/uploads/Taipei-Studio-Apartment_1.jpeg"
       },
       {
         id: "acc37",
         name: "Expo Family Suite",
         price: 200,
-        image: "https://www.familysuites.com/images/family-suite.jpg"
+        image: "https://barunabali.holidayinnresorts.com/wp-content/uploads/2020/07/Family-Suite1-1.jpg"
       },
       {
         id: "acc38",
         name: "Expo Shared Cabin",
         price: 70,
-        image: "https://www.sharedcabins.com/images/shared-cabin.jpg"
+        image: "https://cruiseshipinteriors-europe.com/wp-content/uploads/2019/09/NCL-interior-cabin-light.jpg"
       },
       {
         id: "acc39",
         name: "Expo Glamping Pod",
         price: 140,
-        image: "https://www.glampingpods.com/images/glamping-pod.jpg"
-      },
-      {
-        id: "acc40",
-        name: "Expo Caravan",
-        price: 160,
-        image: "https://www.caravans.com/images/caravan.jpg"
+        image: "https://highoaksgrange.co.uk/images/gallery/accommodation/glamping-pods/pod-1/thumbs/Family_Glamping_Pod_-_00_02_1600.jpg"
       }
     ]
   }
